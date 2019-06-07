6f9da9258ab33ef5589fc59b376788283860d27e
unknown <andrew.lewis.hanson@gmail.com>
2019-06-07 13:34:11 -0700

    here

--- models/index.js
+++ models/index.js
@@ -5,15 +5,13 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV;
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var db        = {};
+var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+  
+
 
 fs
   .readdirSync(__dirname)
